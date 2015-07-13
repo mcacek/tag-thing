3dca2acbcab9f41c7e01f8469d2b6cf7c733ee22
Michael Cacek <michael.cacek@so.mnscu.edu>
2015-07-13 14:56:04 -0500

    MISC - Add develop task with file watchers.

---
+++ src/gulp/tasks/develop.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+//var batch = require('gulp-batch');
+
+function develop(cb) { // eslint-disable-line
+  watch('src/**/*.*', function (events, done) {
+    gulp.start('build');
+  });
+}
+
+gulp.task('develop', develop);
+
+module.exports = {
+  develop: develop
+};
