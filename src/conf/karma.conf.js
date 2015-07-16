c5f8f88d3dc880041c7bd29cf08e2a418bbcbe63
Michael Cacek <michael.cacek@so.mnscu.edu>
2015-07-16 15:48:35 -0500

    MISC - Run unit tests against PhantomJS.
    - Update karma config for phantomjs launcher.
    - Update eslintrc for jasmine globals.
    - Update karma configuration to correctly handle browswerified assets.

--- src/conf/karma.conf.js
+++ src/conf/karma.conf.js
@@ -1,13 +1,25 @@
 module.exports = function(config) {
   config.set({
-    files: ['../../js/**/*.spec.js'],
+    files: [
+      '../../src/js/main.js',
+      '../../node_modules/angular-mocks/angular-mocks.js',
+      '../../src/js/**/*.spec.js'
+    ],
     frameworks: ['browserify', 'jasmine'],
     preprocessors: {
+      '../../src/js/main.js': [ 'browserify' ],
       '../../js/**/*.spec.js': [ 'browserify' ]
     },
+    singleRun: true,
+    browsers: ['PhantomJS'],
     browserify: {
       debug: true,
       transform: [ 'brfs' ]
+      // configure: function(bundle) {
+      //   bundle.on('prebundle', function() {
+      //     // prebundle and stuff
+      //   });
+      // }
     }
   });
 };
