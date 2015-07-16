5b34e32e6e9c9f16a86696a38eb2934dc061b1f2
Michael Cacek <michael.cacek@so.mnscu.edu>
2015-07-16 16:25:33 -0500

    MISC - Add tests for tag service.
    - Add eslint rules for jasmine globals.
    - Add rules that actually test things.

--- src/js/services/tag-service/tag-service.js
+++ src/js/services/tag-service/tag-service.js
@@ -3,27 +3,27 @@ var _ = require('lodash');
 // #TagService
 function service() {
   var methods = {
-    getTagSalenience: getTagSalenience,
+    getTagSalience: getTagSalience,
     getMaxTagCount: getMaxTagCount
   };
 
-  // ##getSalience
+  // ##getTagSalience
   // Given a current tag count as well as min and max count, determine salience
   // value ranging from 1-10.
-  function getTagSalenience(count, max) {
-    return Math.round( (count / max) * 10 );
+  function getTagSalience(count, max) {
+    var salience = Math.round( (count / max) * 10 );
+
+    return (salience < 1) ? 1 : salience;
   }
 
   // #getMaxTagCount
   // Determine the max tag use count given a map of tags.
   function getMaxTagCount(tags) {
-    if (tags.length === 0) {
+    var tagCounts = _.map(tags);
+
+    if (tagCounts.length === 0) {
       return 0;
     }
-
-    var tagCounts = _.map(tags, function(count) {
-      return count;
-    });
 
     return Math.max.apply(null, tagCounts);
   }
--- src/js/services/tag-service/tag-service.spec.js
+++ src/js/services/tag-service/tag-service.spec.js
@@ -1,11 +1,80 @@
 describe('TagService', function() {
+  beforeEach(module('tagThing'));
+
+  var tagService;
+
+  beforeEach(inject(function(_tagService_) {
+    tagService = _tagService_;
+  }));
+
+  describe('#getTagSalience', function() {
+    it('should return 1 if less 1', function() {
+      // given an expectation of a less than 1
+      var returnedSalience;
+      var count = 0;
+      var max = 10;
+
+      // when getTagSalience is called
+      returnedSalience = tagService.getTagSalience(count, max);
+
+      // then we should expect a value of 1
+      expect(returnedSalience).toEqual(1);
+    });
+
+    it('should round up', function() {
+      // given some values that should not return a whole value (.18)
+      var returnedSalience;
+      var count = 2;
+      var max = 11;
+
+      // when getTagSalience is called
+      returnedSalience = tagService.getTagSalience(count, max);
+
+      // then we should expect a rounded up value
+      expect(returnedSalience).toEqual(2);
+    });
+
+    it('should round down', function() {
+      // given some values that should not return a whole value (.14)
+      var returnedSalience;
+      var count = 2;
+      var max = 14;
+
+      // when getTagSalience is called
+      returnedSalience = tagService.getTagSalience(count, max);
+
+      // then we should expect a rounded down value
+      expect(returnedSalience).toEqual(1);
+    });
+  });
+
   describe('getMaxTagCount', function() {
     it('should return 0 if no tags map is empty', function() {
+      // given an empty tag map
+      var tags = {};
+      var returnedTagCount;
 
+      // when getMaxTagCount is called
+      returnedTagCount = tagService.getMaxTagCount(tags);
+
+      // then the tag count should be 0
+      expect(returnedTagCount).toEqual(0);
     });
 
     it('should return the appropriate max count', function() {
+      // given a tag map with real values
+      var tags = {
+        tag1: 5,
+        tag2: 8,
+        tag3: 6
+      };
+      var returnedTagCount;
 
+      // when getMaxTagCount is called
+      returnedTagCount = tagService.getMaxTagCount(tags);
+
+      // then the tag count should be the largest value
+      expect(returnedTagCount).toEqual(8);
     });
   });
 });
